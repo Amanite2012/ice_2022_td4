78190b63d1fc7389d1dfe2b212eafff744bff925
Amanite <antoine.petit@epita.fr>
2022-05-31 11:16:37 +0200

    Post create_coin() with force code

--- project/app/api/coins.py
+++ project/app/api/coins.py
@@ -7,3 +7,7 @@ router = APIRouter()
 @router.get("/", response_model=list[CoinBaseResponseSchema])
 async def read_all_coins() -> list[CoinBaseResponseSchema]:
     return await crud_coins.get_all()
+
+@router.post("/", status_code=201)
+async def create_coin():
+    pass
