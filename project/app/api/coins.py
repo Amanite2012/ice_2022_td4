df3394a0448130963a8441ed1d513da3b8addfcd
Amanite <antoine.petit@epita.fr>
2022-05-31 11:21:44 +0200

    Post create_coin() with post method

--- project/app/api/coins.py
+++ project/app/api/coins.py
@@ -1,6 +1,6 @@
 from app.api import crud_coins
 from fastapi import APIRouter, HTTPException, Path
-from app.models.pydantic import CoinBaseResponseSchema
+from app.models.pydantic import CoinBaseResponseSchema, CoinBaseSchema
 
 router = APIRouter()
 
@@ -9,5 +9,7 @@ async def read_all_coins() -> list[CoinBaseResponseSchema]:
     return await crud_coins.get_all()
 
 @router.post("/", status_code=201)
-async def create_coin():
-    pass
+async def create_coin(payload: CoinBaseSchema) -> CoinBaseResponseSchema:
+    coin = await crud_coins.post(payload)
+    return coin
+
--- project/app/api/crud_coins.py
+++ project/app/api/crud_coins.py
@@ -1,5 +1,12 @@
 from app.models.tortoise import Coin
+from app.models.pydantic import CoinBaseResponseSchema, CoinBaseSchema
+
 
 async def get_all() -> list | None:
     coins = await Coin.all().values()
     return coins
+
+async def post(payload: CoinBaseSchema) -> CoinBaseResponseSchema:
+    coin = Coin(name_id=payload.name_id)
+    await coin.save()
+    return coin
